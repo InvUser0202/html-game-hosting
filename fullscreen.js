b62266710ebc04228ed17bd200e4d5151036d498
InvUser0202 <valentjsus@gmail.com>
2025-05-02 10:36:21 +0100

    Update fullscreen.js

--- fullscreen.js
+++ fullscreen.js
@@ -1,1 +1,11 @@
+document.addEventListener("click", () => {
+    const gameElement = document.documentElement; // Fullscreen applies to the full page
 
+    if (gameElement.requestFullscreen) {
+        gameElement.requestFullscreen();
+    } else if (gameElement.webkitRequestFullscreen) {
+        gameElement.webkitRequestFullscreen();
+    } else if (gameElement.msRequestFullscreen) {
+        console.log("Fullscreen is not supported in this browser.");
+    }
+});
